003164d77afbecb1ced66992626c60188aa172ea
seiay <seiieshima831@gmail.com>
2022-11-22 23:00:03 +0900

    delete documents by local

--- saint-sorting/src/main.rs
+++ saint-sorting/src/main.rs
@@ -4,7 +4,8 @@ use tera::{Tera, Context};
 use serde::{Serialize, Deserialize};
 use actix_identity::{Identity, IdentityMiddleware};
 use actix_session::{storage::CookieSessionStore, SessionMiddleware};
-use firestore_db_and_auth::{documents, documents::List, Credentials, ServiceSession, errors::Result};
+use firestore_db_and_auth::{documents, documents::List, Credentials, 
+                            ServiceSession, errors::Result, errors::FirebaseError};
 
 mod api;
 mod auth_error;
@@ -37,16 +38,7 @@ pub struct FormParams {
 }
 
 
-fn write(session: &ServiceSession) -> Result<()> {
-    let obj = DemoDTO { a_string: "abcd".to_owned(), an_int: 14, another_int: 10 };
-
-    let result = documents::write(session, "ss", Some("service_test"), &obj, documents::WriteOptions::default())?;
-
-    println!("id: {}, created: {}, updated: {}", result.document_id, result.create_time.unwrap(), result.update_time.unwrap());
-    Ok(())
-}
-
-// async fn write_web(session: &ServiceSession) -> Result<()> {
+// fn write(session: &ServiceSession) -> Result<()> {
 //     let obj = DemoDTO { a_string: "abcd".to_owned(), an_int: 14, another_int: 10 };
 
 //     let result = documents::write(session, "ss", Some("service_test"), &obj, documents::WriteOptions::default())?;
@@ -55,12 +47,14 @@ fn write(session: &ServiceSession) -> Result<()> {
 //     Ok(())
 // }
 
-// fn write_partial(session: &ServiceSession) -> Result<()> {
-//     let obj = DemoPartialDTO { a_string: None, an_int: 16 };
-//     let result = documents::write(session, "tests", Some("service_test"), &obj, documents::WriteOptions{merge:true})?;
-//     println!("id: {}, created: {}, updated: {}", result.document_id, result.create_time.unwrap(), result.update_time.unwrap());
-//     Ok(())
-// }
+async fn write_web(session: &ServiceSession) -> Result<()> {
+    let obj = DemoDTO { a_string: "abcd".to_owned(), an_int: 14, another_int: 10 };
+
+    let result = documents::write(session, "ss", Some("service_test_v2"), &obj, documents::WriteOptions::default())?;
+
+    println!("id: {}, created: {}, updated: {}", result.document_id, result.create_time.unwrap(), result.update_time.unwrap());
+    Ok(())
+}
 
 // async fn user(
 //     tmpl: web::Data<Tera>,)
@@ -72,8 +66,6 @@ fn write(session: &ServiceSession) -> Result<()> {
 //     //     .map_err(|e| error::ErrorInternalServerError(e))?;
 
 //     // Ok(HttpResponse::Ok().content_type("text/html").body(view))
-
-
 
 
 async fn top(
@@ -143,11 +135,23 @@ async fn book() -> impl Responder {
     "hello book!"
 }
 
+//test write
+/*
 fn main() -> std::io::Result<()>{
     let cred = Credentials::from_file("firebase-service-account.json").unwrap();
     let auth = ServiceSession::new(cred).unwrap();
 
     write(&auth);
+
+    Ok(())
+}*/
+
+// test delete
+fn main() -> std::io::Result<()>{
+    let cred = Credentials::from_file("firebase-service-account.json").unwrap();
+    let auth = ServiceSession::new(cred).unwrap();
+
+    documents::delete(&auth, "ss/service_test", true);
 
     Ok(())
 }
