b8f0fc2fceab4ad8c1da01194fd189a209d7059a
seiay <seiieshima831@gmail.com>
2022-12-17 21:29:50 +0900

    add write function from clothing page.

--- saint-sorting/src/firestore/write.rs
+++ saint-sorting/src/firestore/write.rs
@@ -1,10 +1,14 @@
 use crate::*;
 
-pub async fn write_firestore(
+pub async fn write_firestore<T>(
     session: Session,
-    params: web::Form<FormParamsDbWrite>,
+    document_id: String,
+    obj: &T,
     tmpl: web::Data<Tera>,) 
-    -> actix_web::Result<HttpResponse, Error> {
+    -> actix_web::Result<HttpResponse, Error> 
+where
+    T: Serialize,
+    {
 
     let user_id = match session.get::<Uuid>("user_id")? {
         None => return Ok(HttpResponse::Unauthorized().finish()),
@@ -13,17 +17,17 @@ pub async fn write_firestore(
 
     let context = Context::new();
     
-    let new_doc_id  =  String::from(&params.document_id);
-    let new_a_string = String::from(&params.a_string);
-    let new_an_int = params.an_int;
-    let new_another_int = params.another_int;
+    // let new_doc_id  =  String::from(&params.document_id);
+    // let new_a_string = String::from(&params.a_string);
+    // let new_an_int = params.an_int;
+    // let new_another_int = params.another_int;
 
-    let obj = DemoDTO { a_string: new_a_string, an_int: new_an_int, another_int: new_another_int};
+    // let obj = DemoDTO { a_string: new_a_string, an_int: new_an_int, another_int: new_another_int};
 
     let cred = Credentials::from_file("firebase-service-account.json").unwrap();
     let auth = ServiceSession::new(cred).unwrap();
 
-    let _result = documents::write(&auth, &user_id, Some(new_doc_id), &obj, documents::WriteOptions::default());
+    let _result = documents::write(&auth, &user_id, Some(document_id), &obj, documents::WriteOptions::default());
 
     // println!("id: {}, created: {}, updated: {}", result.document_id, result.create_time.unwrap(), result.update_time.unwrap());
     
@@ -32,6 +36,40 @@ pub async fn write_firestore(
     
     Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
+ 
+
+// pub async fn write_firestore(
+//     session: Session,
+//     params: web::Form<FormParamsDbWrite>,
+//     tmpl: web::Data<Tera>,) 
+//     -> actix_web::Result<HttpResponse, Error> {
+
+//     let user_id = match session.get::<Uuid>("user_id")? {
+//         None => return Ok(HttpResponse::Unauthorized().finish()),
+//         Some(i) => i.to_string()
+//     };
+
+//     let context = Context::new();
+    
+//     let new_doc_id  =  String::from(&params.document_id);
+//     let new_a_string = String::from(&params.a_string);
+//     let new_an_int = params.an_int;
+//     let new_another_int = params.another_int;
+
+//     let obj = DemoDTO { a_string: new_a_string, an_int: new_an_int, another_int: new_another_int};
+
+//     let cred = Credentials::from_file("firebase-service-account.json").unwrap();
+//     let auth = ServiceSession::new(cred).unwrap();
+
+//     let _result = documents::write(&auth, &user_id, Some(new_doc_id), &obj, documents::WriteOptions::default());
+
+//     // println!("id: {}, created: {}, updated: {}", result.document_id, result.create_time.unwrap(), result.update_time.unwrap());
+    
+//     let view = tmpl.render("db_top.html", &context)
+//         .map_err(|e| error::ErrorInternalServerError(e))?;
+    
+//     Ok(HttpResponse::Ok().content_type("text/html").body(view))
+// }
 
 // for write firestore_db
 #[derive(Serialize, Deserialize)]
