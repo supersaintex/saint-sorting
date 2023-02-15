312066a7febb489915f8d9aeacbed3adc47021e6
seiay <seiieshima831@gmail.com>
2023-02-15 23:55:11 +0900

    23/2/16 show clothing page after writing documetnt at clothing page.

--- saint-sorting/src/firestore/write.rs
+++ saint-sorting/src/firestore/write.rs
@@ -5,27 +5,25 @@ pub async fn write_firestore<T>(
     document_id: String,
     obj: &T,
     tmpl: web::Data<Tera>,) 
-    -> actix_web::Result<HttpResponse, Error> 
+    -> actix_web::Result<HttpResponse, super::firestore_error::FireStoreError>
 where
     T: Serialize,
     {
-
-    //ここを別の関数にする？
+    //session check & unwrap user_id
     let user_id = match session.get::<Uuid>("user_id")? {
         None => return Ok(HttpResponse::Unauthorized().finish()),
         Some(i) => i.to_string()
     };
-
-    let context = Context::new();
 
     let cred = Credentials::from_file("firebase-service-account.json").unwrap();
     let auth = ServiceSession::new(cred).unwrap();
 
     let _result = documents::write(&auth, &user_id, Some(document_id), &obj, documents::WriteOptions::default());
 
+    let context = Context::new();
     let view = tmpl.render("db_top.html", &context)
-        .map_err(|e| error::ErrorInternalServerError(e))?;
-    Ok(HttpResponse::Ok().content_type("text/html").body(view))
+       .map_err(|e| error::ErrorInternalServerError(e))?;
+    Ok(HttpResponse::Ok().content_type("texthtml").body(view))
 }
 
 #[derive(Serialize, Deserialize)]
