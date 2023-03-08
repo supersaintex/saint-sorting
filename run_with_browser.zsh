d15a419c40d294fa1a996b04eb3b09ac7641c7c6
asana17 <kei17.kaight084@gmail.com>
2023-03-09 01:48:58 +0900

    modify run script

--- run_with_browser.zsh
+++ run_with_browser.zsh
@@ -1,9 +1,29 @@
 #!/bin/zsh
 
-# cargo run
-cargo build
+TIMEOUT=300
+count=0
+PID=$$
+cmd="ps aux | grep ' target/debug/saint-sorting' | grep -v grep | wc -l"
+pn=$(eval $cmd)
+if [ $pn -eq 1 ]; then
+  echo "app already running; kill & run..."
+  pid=`ps aux | grep ' target/debug/saint-sorting' | grep -v grep | awk '{print $2}'`
+  kill -9 $pid
+fi
 (
-	sleep 1
-	google-chrome http://127.0.0.1:8080/app/top
+  while :
+  do
+    cmd2="ps $PID | wc -l"
+    pn2=$(eval $cmd2)
+    pn=$(eval $cmd)
+    if [ $pn2 -ne 2 ]; then # cargo run failed
+      exit 1
+    fi
+    if [ $pn -eq 1 ]; then
+      google-chrome http://127.0.0.1:8080/app/top
+      exit 0
+    fi
+    sleep 1
+  done
 )&
-./target/debug/saint-sorting
+cargo run
