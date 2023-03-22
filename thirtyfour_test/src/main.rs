57c3d5ee790b29efefe5b558633c4511c1a905fa
seiay <seiieshima831@gmail.com>
2023-03-22 23:16:59 +0900

    sign in and enter user page by Driver

--- thirtyfour_test/src/main.rs
+++ thirtyfour_test/src/main.rs
@@ -6,7 +6,21 @@ async fn main() -> color_eyre::Result<()> {
 
     let caps = DesiredCapabilities::chrome();
     let driver = WebDriver::new("http://localhost:4444/", caps).await?;
+
+    // Sign in
     driver.goto("https:/127.0.0.1:8080/app/top").await?;
+
+    let signin_form = driver.find(By::Id("signInForm")).await?;
+    let signin_email = signin_form.find(By::Id("email-2")).await?;
+    //input each mail address
+    signin_email.send_keys("seiieshima831@gmail.com").await?;
+
+    let signin_password = signin_form.find(By::Id("password-2")).await?;
+    //input each password
+    signin_password.send_keys("123456").await?;
+
+    let signin_buttom = driver.find(By::Id("button-2")).await?;
+    signin_buttom.click().await?;
 
     Ok(())
 }
