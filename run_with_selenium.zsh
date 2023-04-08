5c2da15885f9d279230491e2583f4593228c3dbf
seiay <seiieshima831@gmail.com>
2023-04-08 12:47:13 +0900

    Update script

--- run_with_selenium.zsh
+++ run_with_selenium.zsh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 {
+	cd thirtyfour_test
+	java -jar *.jar --ext example.jar:dir standalone
+}&
+{
 	./run_with_browser.zsh
 }&
-
 {
 	cd thirtyfour_test
 	sleep 1
@@ -10,10 +13,12 @@
 	do
 		cmd="ps aux | grep ' target/debug/saint-sorting' | grep -v grep | wc -l"
 		pn=$(eval $cmd)
-		if [ $pn -eq 1 ]; then
-		  	cargo run
+		cmd2="ps aux | grep ' selenium' | grep -v grep | wc -l"
+		pn2=$(eval $cmd2)
+		if [ $pn -eq 1 ] && [ $pn2 -eq 1 ]; then
+			cargo run
 			exit 0
 		fi
-		  	sleep 1
+		sleep 1
 	done
 }&
