d5a5339695c9c2b9e9725a45cbcb0a7e5e052132
seiay <seiieshima831@gmail.com>
2023-04-17 23:35:17 +0900

    kill app & selemium after thirtyfour_test ends

--- run_with_selenium.zsh
+++ run_with_selenium.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-#TIMEOUT=300
-#count=0
-#PID=$$
+TIMEOUT=300
+count=0
+PID=$$
 #cmd="ps aux | grep ' target/debug/saint-sorting' | grep -v grep | wc -l"
 #pn=$(eval $cmd)
 #if [ $pn -eq 1 ]; then
@@ -26,13 +26,22 @@
 #  done
 #)&
 #echo "hello"
-{
+(
 	cd thirtyfour_test
 	java -jar *.jar --ext example.jar:dir standalone
-}&
+)&
 {
-	./run_without_browser.zsh
-	#./run_with_browser.zsh
+	sleep 1
+	while :
+	do
+		cmd3="ps aux | grep ' selenium' | grep -v grep | wc -l"
+		pn3=$(eval $cmd3)
+		if [ $pn3 -eq 1 ]; then
+			./run_without_browser.zsh
+			exit 0
+		fi
+		sleep 1
+	done
 }&
 {
 	cd thirtyfour_test
@@ -50,6 +59,22 @@
 		sleep 1
 	done
 }&
-#cargo run &
-#cd thirty_four
-#cargo run
+# kill application and selenium when the test ends.
+sleep 3
+while :
+do
+	cmd="ps aux | grep ' target/debug/saint-sorting' | grep -v grep | wc -l"
+	pn=$(eval $cmd)
+	cmd2="ps aux | grep ' target/debug/thirtyfour_test' | grep -v grep | wc -l"
+	pn2=$(eval $cmd2)
+	if [ $pn -eq 1 ] && [ $pn2 -eq 0 ]; then
+		echo "The test ends, and kill the application and selenium server..."
+		sleep 3
+		pid=`ps aux | grep ' target/debug/saint-sorting' | grep -v grep | awk '{print $2}'`
+		kill -9 $pid
+		pid2=`ps aux | grep ' selenium' | grep -v grep | awk '{print $2}'`
+		kill -9 $pid2
+		exit 0
+	fi
+	sleep 1
+done
